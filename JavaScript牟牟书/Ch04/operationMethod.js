5b5ed007555c09df339771350c6ab951dde232cc
xiaofanzy <15755156769@163.com>
2023-05-23 13:24:37 +0800

    chap04

--- JavaScript牟牟书/Ch04/operationMethod.js
+++ JavaScript牟牟书/Ch04/operationMethod.js
@@ -23,6 +23,7 @@ console.log(options.verbose ?? true); // false
 console.log(options.n ?? 10); // 10
 //如果混用了？？和|| 两个操作符，那么他必须使用括号说明哪个先执行，否则会报错；
 // a ?? b || c // SyntaError;
+let c;
 (a ?? b) || c; //这个是可以的；
 
 /**
@@ -31,6 +32,29 @@ console.log(options.n ?? 10); // 10
 let value = "n";
 //如果是string,则包含在引号中。
 (typeof value === 'string') ? "'" + value + "'" : value.toString();
+
+// delete
+let o = { x: 1, y: 2 };
+delete o.x;
+console.log("x" in o);
+
+a = [1, 2, 3];
+delete a[2];    //删除数组的最后一位元素，但是保留长度；
+console.log(a.length);  //3
+
+/**
+ * delete 在普通模式下返回的是true
+ * 严格模式下， 当删除变量函数，函数参数的时候，就回包 SyntaError 
+ * 尝试删除不可配置的属性的时候，抛出TypeError;
+ */
+o = { x: 1, y: 2 };
+delete o.x; //删除
+typeof o.x; // undefined
+delete o.x; // true ,始终返回true
+delete 1;   // 无意思，但是还是会返回true
+delete o;   //不能删除变量；
+delete Object.prototype;    //不可删除的属性；
+
 
 
 
