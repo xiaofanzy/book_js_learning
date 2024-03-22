0184b7798a55f933b37026e7fb92775b4ce592fe
zhangyu <zhangyu2@tellhow.com>
2024-03-22 14:31:35 +0800

    code delete beacuse i allready

--- JavaScript牟牟书/Ch03/date.js
+++
@@ -1,8 +0,0 @@
-// 当前时间的时间戳
-let timestamp = Date.now();
-//当前日期
-let now = new Date();
-//毫秒时间戳
-let ms = now.getTime();
-let ios = now.toISOString();
-
--- JavaScript牟牟书/Ch06/know.md
+++
@@ -1,6 +0,0 @@
-## 对象简介
-### 属性值
-- writable 可写
-- enumerable 可枚举
-- configurable 可配制
-
