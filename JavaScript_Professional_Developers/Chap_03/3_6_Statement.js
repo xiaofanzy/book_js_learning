7164e9f74f769f59d71ad37c705f30343d32971d
zhangyu <zhangyu2@tellhow.com>
2024-03-25 09:05:11 +0800

    code

--- JavaScript_Professional_Developers/Chap_03/3_6_Statement.js
+++ JavaScript_Professional_Developers/Chap_03/3_6_Statement.js
@@ -21,3 +21,39 @@ for (const i of [2, 3, 4, 5, 6]) {
 }
 
 // 标签语句 gpt说可以指定跳出,类似于c 里面的goto
+start: for (let i = 0; i < count; i++) {
+  console.log(i);
+}
+
+// break, continue
+let num = 0;
+for (let i = 0; i < 10; i++) {
+  if (i % 5 == 0) {
+    continue;
+  }
+}
+
+// break 跳出循环到当前循环
+outermost: for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    break outermost;
+  }
+  num++;
+}
+console.log(num);
+
+outmosts: for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    if (i == 5 && j == 5) {
+      continue outmosts;
+    }
+  }
+}
+
+// 这个就相当于
+let qs = location.search.substring();
+with (location) {
+  let qs = search.substring();
+  let hostName = hostName;
+  let url = href;
+}
