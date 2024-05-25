0b2002d674b4a118b1d7929ab04eaef09341e668
zhangyu <zhangyu2@tellhow.com>
2024-05-25 18:03:36 +0800

    feview books

--- JavaScript_Professional_Developers/Chap_03/3_7_function.js
+++
@@ -1,9 +0,0 @@
-/**  函数*/
-
-function functionName(arg0, arg1, ...argN) {}
-
-function sayHi(name, message) {
-  console.log("Hello", name, message);
-}
-
-sayHi("tom", "how are you today?");
--- JavaScript_Professional_Developers/Chap_07/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-function* generatorF2() {
-  return yield "foo";
-}
-
-let fooss = generatorF2();
-console.log(fooss.next());
-console.log(fooss.next("bar"));
